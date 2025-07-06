b7054a152a1062c6aca4afee1cfe5f72acfde0dc
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-07-06 10:33:52 -0400

    Update rustscan_&_nmap.sh
    removed redundant nmap option

--- Enumeration/rustscan_&_nmap.sh
+++ Enumeration/rustscan_&_nmap.sh
@@ -2,5 +2,5 @@
 rustscan -a $IP -- -oN rusty
 
 # nmap scan on open ports
-nmap -p $(awk '/tcp/ {print $1}' rusty | tr '\n' ',' | sed 's/\/tcp//g' | sed 's/,$//g') -sC -sV -O -A -oN nmap $IP
+nmap -p $(awk '/tcp/ {print $1}' rusty | tr '\n' ',' | sed 's/\/tcp//g' | sed 's/,$//g') -sC -sV -A -oN nmap $IP
 
