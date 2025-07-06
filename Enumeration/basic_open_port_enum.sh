46e71e74f1feebb42fa1f177d1e6515bcbafd36d
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-07-06 12:24:49 -0400

    Update basic_open_port_enum.sh

--- Enumeration/basic_open_port_enum.sh
+++ Enumeration/basic_open_port_enum.sh
@@ -1,9 +1,12 @@
+echo [+] Starting rustscan with default options for $IP
+
 # standard open port scan
 rustscan -a $IP -- -oN rusty
 
 # show the rustscan output
 less -R rusty
 
+echo [+] Starting nmap to probe versions, and OS and assuming host is online.
 # start nmap scan on open ports
-nmap -vv -p $(awk '/tcp/ {print $1}' rusty | tr '\n' ',' | sed 's/\/tcp//g' | sed 's/,$//g') -sV -Pn -A -oN nmap $IP
+nmap -vv -p $(awk '/tcp/ {print $1}' rusty | tr '\n' ',' | sed 's/\/tcp//g' | sed 's/,$//g') -sV -Pn -O -oN nmap $IP
 
