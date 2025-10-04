fb0128732afad79a2307fedf66591ab3bdee5888
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-10-04 12:46:43 -0400

    Update new_kali_script.sh

--- set_up_new_kali_vm/new_kali_script.sh
+++ set_up_new_kali_vm/new_kali_script.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+sudo apt install pipx git -y
 pipx install name-that-hash dirsearch
 pip3 install wsgidav --break-system-packages
-pipx install netexec
+pipx ensurepath
+pipx install git+https://github.com/Pennyw0rth/NetExec
