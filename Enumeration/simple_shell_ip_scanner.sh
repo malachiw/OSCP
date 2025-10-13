4827e5a00da35f25b680c8cdc301b71481407337
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-10-13 12:13:49 -0400

    Update simple_shell_ip_scanner.sh

--- Enumeration/simple_shell_ip_scanner.sh
+++ Enumeration/simple_shell_ip_scanner.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-#scan a /24 cidr range with basic sh tools.
+#scan a /24 cidr range with basic sh tools. This scan searches
+# for machines that have port 445 open.
 for i in $(seq 1 254); do nc -zv -w 1 172.16.169.$i 445; done
