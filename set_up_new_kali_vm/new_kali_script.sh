4343eaf96fbb6a712b9a1d8c610ae745b24a995d
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-10-17 21:20:15 -0400

    Update new_kali_script.sh

--- set_up_new_kali_vm/new_kali_script.sh
+++ set_up_new_kali_vm/new_kali_script.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-sudo apt install pipx git feroxbuster -y
+sudo apt install pipx git feroxbuster ncat -y
 pipx install name-that-hash dirsearch
 pip3 install wsgidav --break-system-packages
 pipx ensurepath
