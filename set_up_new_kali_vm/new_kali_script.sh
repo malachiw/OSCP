b82977361bbf3d6ae1d7a48b31e74b316543f2a1
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-10-19 13:15:08 -0400

    Update new_kali_script.sh

--- set_up_new_kali_vm/new_kali_script.sh
+++ set_up_new_kali_vm/new_kali_script.sh
@@ -5,3 +5,5 @@ pipx install name-that-hash dirsearch
 pip3 install wsgidav --break-system-packages
 pipx ensurepath
 pipx install git+https://github.com/Pennyw0rth/NetExec
+sudo msfdb init
+sudo systemctl enable postgresql
