f52fb684b0e9a773d0af7b45485e306df34df71d
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-10-28 18:02:58 -0400

    manually check 1 password at a time

---
+++ Windows/pwd_ck.ps1
@@ -0,0 +1,12 @@
+param (
+	[string]$Arg1,
+	[string]$Arg2
+)
+
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+New-Object System.DirectoryServices.DirectoryEntry($SearchString, $Arg1, $Arg2)	
