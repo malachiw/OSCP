ff37a68166bad97fff690bfd535e05c7ea77a72c
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-11-01 10:45:24 -0400

    Update new_kali_script.sh

--- set_up_new_kali_vm/new_kali_script.sh
+++ set_up_new_kali_vm/new_kali_script.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-sudo apt install pipx git feroxbuster ncat chisel neo4j bloodhound crackmapexec -y
+sudo apt install pipx git feroxbuster ncat chisel neo4j bloodhound crackmapexec shellter wine -y
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine32
 pipx install name-that-hash dirsearch
 pip3 install wsgidav --break-system-packages
 pipx ensurepath
