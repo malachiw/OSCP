37715a0722ab64ac09bbc42cbb8fe5a1343fcb90
Malachi Woodlee <malachi.woodlee@gmail.com>
2025-11-08 09:54:52 -0500

    Update new_kali_script.sh

--- set_up_new_kali_vm/new_kali_script.sh
+++ set_up_new_kali_vm/new_kali_script.sh
@@ -193,26 +193,22 @@ install_add_creds() {
 #!/bin/zsh
 
 # Ensure two arguments are passed
-if [[ "$#" -ne 2 ]]; then
-    echo "Usage: add_creds <username:password> <path>"
-    exit 1
-fi
-
-# Ensure the first argument contains a colon
-if [[ "$1" != *:* ]]; then
-    echo "Error: First argument must be in the format username:password"
-    echo "Usage: add_creds <username:password> <path>"
+if [[ "$#" -ge 3 ]]; then
+    echo "Usage: add_creds <username> <password> <path> <notes (optional)>"
     exit 1
 fi
 
 # Ensure the destination file exists or can be created
-if ! touch "$2" 2>/dev/null; then
+if ! touch "$3" 2>/dev/null; then
     echo "Error: Cannot write to $2"
     exit 1
 fi
 
 # Append creds to cred file
-echo "$1" >> "$2"
+if [[ "$#" == 3 ]]; then
+  echo "$1:$2" >> "$3"
+else
+   echo "$1:$2 $4" >> "$3"
 echo "Credentials added to $2"
 EOF
 
